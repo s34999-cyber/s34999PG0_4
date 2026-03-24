bc01470531961124da1b3ade3771b52d7b427fbc
Vincent Radecki <s34999@pjwstk.edu.pl>
2026-03-24 11:21:24 +0100

    Initialize project, Task1 Menu Item

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        MenuItem m1 = new MenuItem("C-01","Tiramisu",13.99,"Cake");
+        MenuItem m2 = new MenuItem("P-01","Croissant",4.25,"Pastry");
+        MenuItem m3 = new MenuItem("C-02","Cheesecake",12.00,"Cake");
+        MenuItem m3copy = new MenuItem("C-02","Cheesecake Part 2",12.00,"Cake");
+        System.out.println(MenuItem.getProductCount());
+        System.out.println(m3);
+        System.out.println(m3.equals(m1));
+        System.out.println(m3.equals(m3copy));
+
+    }
+}
